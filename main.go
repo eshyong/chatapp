f90421aa98c719f414d799d9aa65082b2fe11ffb
Eric Shyong <eric.shyong@gmail.com>
2017-01-06 19:06:07 -0800

    Fix some paths

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 )
 
 const staticDir = "/static/"
+var fileServerPath = filepath.Join(".", staticDir)
 
 func main() {
 	router := setupRoutes()
@@ -20,12 +21,12 @@ func main() {
 
 func setupRoutes() *mux.Router {
 	r := mux.NewRouter()
-	fileServerPath := filepath.Join(".", staticDir)
 	r.PathPrefix(staticDir).Handler(http.StripPrefix(staticDir, http.FileServer(http.Dir(fileServerPath))))
 	r.HandleFunc("/", serveHomePage)
 	return r
 }
 
 func serveHomePage(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./public/html/index.html")
+
+	http.ServeFile(w, r, filepath.Join(fileServerPath, "html", "index.html"))
 }
