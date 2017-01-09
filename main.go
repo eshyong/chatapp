7990c061847449a9d66dbe486e2e817f481c01c6
Eric Shyong <eric.shyong@gmail.com>
2017-01-09 00:32:29 -0800

    It works! TODO: refactor

--- main.go
+++ main.go
@@ -3,19 +3,27 @@ package main
 import (
 	"github.com/gorilla/mux"
 	"net/http"
-	"fmt"
 	"path/filepath"
+	"github.com/gorilla/websocket"
+	"log"
 )
 
 const staticDir = "/static/"
 var fileServerPath = filepath.Join(".", staticDir)
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+}
+var chatRoom = &ChatRoom{
+	chatUsers: []*ChatUser{},
+}
 
 func main() {
 	router := setupRoutes()
 	http.Handle("/", router)
 
-	addr := "localhost:8080"
-	fmt.Println("Starting server on " + addr)
+	addr := ":8080"
+	log.Println("Starting server on " + addr)
 	http.ListenAndServe(addr, nil)
 }
 
@@ -23,10 +31,81 @@ func setupRoutes() *mux.Router {
 	r := mux.NewRouter()
 	r.PathPrefix(staticDir).Handler(http.StripPrefix(staticDir, http.FileServer(http.Dir(fileServerPath))))
 	r.HandleFunc("/", serveHomePage)
+	r.HandleFunc("/chat-room", acceptChatConnection)
 	return r
 }
 
 func serveHomePage(w http.ResponseWriter, r *http.Request) {
-
 	http.ServeFile(w, r, filepath.Join(fileServerPath, "html", "index.html"))
+}
+
+func acceptChatConnection(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	chatRoom.handleUser(conn)
+}
+
+type ChatUser struct {
+	userName string
+	userConn *websocket.Conn
+}
+
+type ChatRoom struct {
+	chatUsers []*ChatUser
+}
+
+// TODO: create a real chat protocol
+func (c *ChatRoom) handleUser(conn *websocket.Conn) {
+	log.Println("User connected from " + conn.RemoteAddr().String())
+	messageType, message, err := conn.ReadMessage()
+	if err != nil || messageType != websocket.TextMessage {
+		log.Println("Unable to authenticate user. Error:")
+		log.Println(err)
+		return
+	}
+	name := string(message)
+	log.Println("Authenticated user: " + name)
+	chatUser := &ChatUser{
+		userName: name,
+		userConn: conn,
+	}
+	c.chatUsers = append(c.chatUsers, chatUser)
+
+	// Handle user connections in a separate goroutine
+	go func() {
+		defer conn.Close()
+		for {
+			messageType, message, err := conn.ReadMessage()
+			if err != nil {
+				log.Println(err)
+				break
+			}
+
+			if messageType != websocket.TextMessage {
+				// Send an error to the user
+				chatUser.userConn.WriteMessage(websocket.TextMessage, []byte("Unable to handle binary messages"))
+				continue
+			}
+
+			c.broadcastMessage(chatUser.userName, message)
+		}
+	}()
+}
+
+func (c *ChatRoom) broadcastMessage(from string, message []byte) {
+	log.Println("Sending message from: " + from)
+	for _, user := range c.chatUsers {
+		// Avoid broadcasting message to sender
+		if user.userName != from {
+			log.Println("Sending message to: " + user.userName)
+			if err := user.userConn.WriteMessage(websocket.TextMessage, message); err != nil {
+				log.Println("Unable to send message to " + user.userName)
+				continue
+			}
+		}
+	}
 }
--- static/js/index.js
+++ static/js/index.js
@@ -3,6 +3,7 @@
 $(document).ready(function () {
     const enterKey = 13;
     var username = '';
+    var chatConn;
 
     $('.username-submit').on('keyup', function(event) {
         var usernameBox = $(event.target);
@@ -15,13 +16,36 @@ $(document).ready(function () {
         username = usernameBox.val();
         usernameBox.fadeOut();
         chatContainer.fadeIn();
+
+        createChatConnection();
     });
+
+    function createChatConnection() {
+        if (!window.WebSocket) {
+            console.log("Your browser doesn't support WebSockets, sorry");
+            return;
+        }
+
+        chatConn = new window.WebSocket('ws://' + window.location.host + '/chat-room');
+        chatConn.onmessage = function(event) {
+            var chatMessages = $('.chat-messages');
+            var chatWindow = $('.chat-window');
+            var message = event.data;
+
+            console.log('Server said: ' + event.data);
+            chatMessages.append('<div class="chat-message">' + message + '</div>');
+            chatWindow.scrollTop(chatMessages.height());
+        };
+        chatConn.onopen = function() {
+            chatConn.send(username);
+        }
+    }
 
     $('.chatbox').on('keyup', function(event) {
         var chatMessages = $('.chat-messages');
         var chatWindow = $('.chat-window');
         var chatBox = $(event.target);
-        var contents = event.target.value.trim('');
+        var contents = event.target.value.trim();
         var message;
 
         if (event.keyCode !== enterKey) {
@@ -35,6 +59,7 @@ $(document).ready(function () {
 
         // Display message, scroll to bottom, and clear chatbox
         message = username + ': ' + contents;
+        chatConn.send(message);
         chatMessages.append('<div class="chat-message">' + message + '</div>');
         chatWindow.scrollTop(chatMessages.height());
         chatBox.val('');
