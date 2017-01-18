44b31bb7d326bf9e36bdaff72badf943d81a49da
Eric Shyong <eric.shyong@gmail.com>
2017-01-18 09:32:45 -0800

    Setup main app server on HTTPS, with an HTTP server redirecting to the main server

--- main.go
+++ main.go
@@ -4,18 +4,70 @@ import (
 	"crypto/tls"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/eshyong/chatapp/chatserver"
-	_ "github.com/lib/pq"
 )
 
 func main() {
+	httpPort := os.Getenv("CHATAPP_HTTP_PORT")
+	if httpPort == "" {
+		httpPort = "8080"
+	}
+	httpsPort := os.Getenv("CHATAPP_HTTPS_PORT")
+	if httpsPort == "" {
+		httpsPort = "8443"
+	}
 
+	// Create a wait group to manage cleanup of both server routines
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		runAppServer(wg, httpsPort)
+	}()
+	go func() {
+		runRedirectingServer(wg, httpPort, httpsPort)
+	}()
+	wg.Wait()
+}
+
+// Creates an HTTPS server which runs the main app
+func runAppServer(wg *sync.WaitGroup, httpsPort string) {
+	// Make sure the WaitGroup is signaled when the server object gets cleaned up
+	defer wg.Done()
+
+	// TLS setup
+	tlsConfig := createDefaultTlsConfig()
+	certFile := os.Getenv("CHATAPP_TLS_CERTIFICATE")
+	keyFile := os.Getenv("CHATAPP_TLS_PRIVATE_KEY")
+
+	if certFile == "" || keyFile == "" {
+		log.Fatal("Need to set CHATAPP_TLS_PRIVATE_KEY and CHATAPP_TLS_CERTIFICATE to continue")
+	}
+
+	// App setup
+	chatServer := chatserver.NewDefaultServer()
+	server := &http.Server{
+		Addr:         ":" + httpsPort,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		TLSConfig:    tlsConfig,
+		Handler:      chatServer.SetupRouter(),
+	}
+	log.Println("Starting HTTPS server on " + server.Addr)
+	if err := server.ListenAndServeTLS(certFile, keyFile); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createDefaultTlsConfig() *tls.Config {
 	// TLS config taken from Filippo Valsorda's blog post:
 	// https://blog.cloudflare.com/exposing-go-on-the-internet/
-	tlsConfig := &tls.Config{
+	return &tls.Config{
 		PreferServerCipherSuites: true,
 		CurvePreferences: []tls.CurveID{
 			tls.CurveP256,
@@ -28,28 +80,33 @@ func main() {
 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 		},
 	}
+}
 
-	chatServer := chatserver.NewDefaultServer()
-	addr := ":8443"
-	if os.Getenv("CHATAPP_ADDR") != "" {
-		addr = os.Getenv("CHATAPP_ADDR")
-	}
-	if os.Getenv("CHATAPP_TLS_PRIVATE_KEY") == "" || os.Getenv("CHATAPP_TLS_CERTIFICATE") == "" {
-		log.Fatal("Need to set CHATAPP_TLS_PRIVATE_KEY and CHATAPP_TLS_CERTIFICATE to continue")
-	}
-	certFile := os.Getenv("CHATAPP_TLS_CERTIFICATE")
-	keyFile := os.Getenv("CHATAPP_TLS_PRIVATE_KEY")
+// Creates an HTTP server which redirects all requests to the main HTTPS server
+func runRedirectingServer(wg *sync.WaitGroup, httpPort, httpsPort string) {
+	defer wg.Done()
 
 	server := &http.Server{
-		Addr:         addr,
+		Addr:         ":" + httpPort,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
-		TLSConfig:    tlsConfig,
-		Handler:      chatServer.SetupRouter(),
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Connection", "close")
+			redirectUrl := &url.URL{}
+			redirectUrl.Scheme = "https"
+			redirectUrl.Host = r.Host
+			if strings.Contains(r.Host, ":") {
+				// Redirect all HTTP requests to the HTTPS server.
+				// If the developer set a custom HTTP port, reconstruct the host:port combination
+				// using the given HTTPS address.
+				redirectUrl.Host = strings.Split(r.Host, ":")[0] + ":" + httpsPort
+			}
+			log.Println(redirectUrl.String())
+			http.Redirect(w, r, redirectUrl.String(), http.StatusMovedPermanently)
+		}),
 	}
-
-	log.Println("Starting server on " + server.Addr)
-	if err := server.ListenAndServeTLS(certFile, keyFile); err != nil {
-		log.Fatalln(err)
+	log.Println("Starting HTTP server on " + server.Addr)
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
 	}
 }
