a6386eba642a4dca447b17d715b1da46c8131907
Eric Shyong <eric.shyong@gmail.com>
2017-02-15 11:51:23 -0800

    Compiling works again

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,22 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import ChatApp from './ChatApp';
-import Login from './Login';
 
 import './index.css';
 
-function main() {
-  let historyType = hashHistory;
-  if (process.env.NODE_ENV === 'production') {
-    historyType = browserHistory;
-  }
-
-  ReactDOM.render((
-    <Router history={historyType}>
-      <Route path="/" component={ChatApp}/>
-      <Route path="/login" component={withRouter(Login)}/>
-    </Router>
-  ), document.getElementById('root'));
-}
-
-main();
+ReactDOM.render(<ChatApp/>, document.getElementById('root'));
