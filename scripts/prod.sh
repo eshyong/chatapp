13a112a68fd10ff6c74359fe73ff40556523f501
Eric Shyong <eric.shyong@gmail.com>
2017-02-21 07:14:54 +0000

    Add frontend build step to prod script

--- scripts/prod.sh
+++ scripts/prod.sh
@@ -2,5 +2,14 @@
 
 set -ex
 
+# Source environment variables
 source ./env_vars
+
+# Build frontend code
+pushd frontend
+npm run build
+popd
+
+# Make and run go server
+make fetchdeps && make
 sudo -E bash -c "$(which chatapp)"
